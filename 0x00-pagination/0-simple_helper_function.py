e1ee8c86cf8cd2e279046aa475bae77c953e2e86
mbentaifour <bentaifourmoh@gmail.com>
2024-10-28 22:42:38 +0000

    Pagination in REST API Design

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Simple helper function
+a function named index_range that takes two integer
+arguments page and page_size
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Page numbers are 1-indexed, the first page is page 1
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
