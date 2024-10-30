b4d2151945e84cfdaec68987b725efd1a979237e
mbentaifour <bentaifourmoh@gmail.com>
2024-10-30 16:03:11 +0000

    BasicCaching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-
-"""Basic dictionary"""
+""" Basic dictionary """
 
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
-    """basic caching system"""
+    """ Basic caching system """
 
     def put(self, key, item):
-        """puts an item in the cache"""
+        """ Puts an item in the cache """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """gets an item from the cache"""
+        """ Gets an item from the cache """
         return self.cache_data.get(key, None)
