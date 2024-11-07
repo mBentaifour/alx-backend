6e35a11ad43af808469cd6ccd4329bb9d99e90cf
mbentaifour <bentaifourmoh@gmail.com>
2024-11-07 14:37:53 +0000

    i18n

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-"""
-Basic Babel setup flask app
-"""
+"""Basic Babel setup"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
+
 
 class Config:
     """
@@ -14,15 +13,15 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
 def hello_world():
-    """
-    to run on the website
-    """
+    """to run on the website"""
     return render_template('1-index.html')
 
 
